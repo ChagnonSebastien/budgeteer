e7e998dda9ecfeaabc492e722389bb14be5c361b
Sebastien Chagnon <sebastien.chagnon@qohash.com>
2025-06-23 13:41:52 -0400

    chore: Converted to nix shell to nix flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A Nix flake providing the project development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.go
+        pkgs.sqlc
+        pkgs.grpc-tools
+        pkgs.nodejs
+        pkgs.postgresql.dev
+      ];
+
+      shellHook = ''
+        if [ -f .env ]; then
+          export $(grep -v '^#' .env | xargs)
+        fi
+
+        export GOROOT=${pkgs.go}/share/go
+        export GOBIN=$PWD/bin
+        export GOPROXY=https://proxy.golang.org,direct
+        export GOSUMDB=sum.golang.org
+        mkdir -p $GOBIN
+
+        export CPATH="${pkgs.postgresql.dev}/include/server:$CPATH"
+        export LIBRARY_PATH="${pkgs.postgresql.dev}/lib:$LIBRARY_PATH"
+        export PATH="$GOBIN:$PATH"
+
+        go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+        go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+        mkdir -p node_modules
+        npm install --prefix ./ @protobuf-ts/plugin
+      '';
+    };
+  };
+}
+
